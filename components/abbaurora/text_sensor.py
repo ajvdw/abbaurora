876e3409ea5f788d681b568ddc56f5483bae19a1
AJ <ajvandewerken@gmail.com>
2021-12-23 19:24:13 +0100

    Update text_sensor.py

--- components/abbaurora/text_sensor.py
+++ components/abbaurora/text_sensor.py
@@ -6,13 +6,13 @@ from esphome.const import (
     CONF_SENSOR,
     ICON_EMPTY,
 )
-from . import ABBAURORA, CONF_ABBAURORA_ID
+from . import ABBAurora, CONF_ABBAURORA_ID
 
 AUTO_LOAD = ["abbaurora"]
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_ABBAURORA_ID): cv.use_id(ABBAURORA),
+        cv.GenerateID(CONF_ABBAURORA_ID): cv.use_id(ABBAurora),
         cv.Optional("identification"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),

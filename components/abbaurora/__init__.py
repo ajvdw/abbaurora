bba56b1607be37ff63fd033c2ac5f1f2c790aefc
AJ <ajvandewerken@gmail.com>
2021-12-24 18:16:19 +0100

    Update __init__.py

--- components/abbaurora/__init__.py
+++ components/abbaurora/__init__.py
@@ -60,15 +60,6 @@ async def to_code(config):
         cg.add(var.set_address(config[CONF_ADDRESS]))
 
 
-
-    for conf in config["abbaurora"]:
-        if CONF_ABB_CUMULATED_ENERGY_TOTAL in conf:
-            sens = await sensor(config[CONF_ABB_CUMULATED_ENERGY_TOTAL])
-            cg.add(var.set_cumulated_energy_total_sensor(sens))
-            var = cg.new_Pvariable(conf[CONF_ID])
-            await cg.register_component(var, conf)
-            await text_sensor.register_text_sensor(var, conf)
-
 #CONF_ABB_V_IN_1 = "v_in_1"
 #CONF_ABB_V_IN_2 = "v_in_2"
 #CONF_ABB_I_IN_1 = "i_in_1"

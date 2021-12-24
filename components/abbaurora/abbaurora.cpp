524403e6f8f48b30d1cf044e2afa63a1c11a775e
AJ <ajvandewerken@gmail.com>
2021-12-24 14:08:12 +0100

    Update abbaurora.cpp

--- components/abbaurora/abbaurora.cpp
+++ components/abbaurora/abbaurora.cpp
@@ -23,7 +23,7 @@ void ABBAuroraComponent::setup()
     connection_status->publish_state("Disconnected");
 }
 
-void AuroraComponentComponent::dump_config() {
+void AuroraComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "inverter:");
   LOG_UART_DEVICE(this);
 

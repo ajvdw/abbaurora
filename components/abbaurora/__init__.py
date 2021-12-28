c5ac89d91d895c5ed1563ac36a25e849857973c5
AJ <ajvandewerken@gmail.com>
2021-12-28 12:24:46 +0100

    Update __init__.py

--- components/abbaurora/__init__.py
+++ components/abbaurora/__init__.py
@@ -41,13 +41,10 @@ async def to_code(config):
     cg.add_global(abbaurora_ns.using)
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
     await uart.register_uart_device(var, config)
-
     if CONF_FLOW_CONTROL_PIN in config:
         pin = await gpio_pin_expression(config[CONF_FLOW_CONTROL_PIN])
         cg.add(var.set_flow_control_pin(pin))
-
     if CONF_ADDRESS in config:
         cg.add(var.set_address(config[CONF_ADDRESS]))
 

a84d24b88968625ff03c224c1daf08297902e0d9
AJ <ajvandewerken@gmail.com>
2021-12-29 14:46:08 +0100

    Update abbaurora.h

--- components/abbaurora/abbaurora.h
+++ components/abbaurora/abbaurora.h
@@ -118,7 +118,6 @@ private:
     static std::string DcDcStateText(uint8_t id);
     static std::string InverterStateText(uint8_t id);
     static std::string AlarmStateText(uint8_t id);
-    static std::string TransmissionStateText(uint8_t id);
 
     union {
         uint8_t asBytes[4];
